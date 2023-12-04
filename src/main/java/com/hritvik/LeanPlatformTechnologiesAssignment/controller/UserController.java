7c1b666eefe2c6a768e1c5079527974a4d094294
Hritvik Soni <hritviksoni768@gmail.com>
2023-12-04 17:32:32 +0530

    added testcases

--- src/main/java/com/hritvik/LeanPlatformTechnologiesAssignment/controller/UserController.java
+++ src/main/java/com/hritvik/LeanPlatformTechnologiesAssignment/controller/UserController.java
@@ -2,6 +2,7 @@ package com.hritvik.LeanPlatformTechnologiesAssignment.controller;
 
 import com.hritvik.LeanPlatformTechnologiesAssignment.model.Users;
 import com.hritvik.LeanPlatformTechnologiesAssignment.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody Users user) throws NoSuchAlgorithmException {
+    public ResponseEntity<String> registerUser( @Valid @RequestBody Users user) throws NoSuchAlgorithmException {
         return userService.registerUser(user);
     }
 
